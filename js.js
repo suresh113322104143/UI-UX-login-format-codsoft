febe4ae94fcd8d82faa877522f7c3cdcc99bfa94
suresh113322104143 <125141865+suresh113322104143@users.noreply.github.com>
2024-07-07 21:32:40 +0530

    js @codesoft menu

---
+++ js.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll("nav a");
+
+  links.forEach(link => {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+});
